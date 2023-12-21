da27eb309762565cd5bad5b303bf1caca0876579
PranavSingh2410 <pranavks223@gmail.com>
2023-12-21 23:22:25 +0530

    1st commit

---
+++ JenkinsPractice1/src/test/java/TestData/Script1.java
@@ -0,0 +1,15 @@
+package TestData;
+
+import org.testng.annotations.Test;
+
+public class Script1 {
+
+	@Test
+	public  void M1() 
+	{
+		System.out.println(" jenkins start execution");
+		
+		System.out.println("jenkins exection done");
+	}
+
+}
